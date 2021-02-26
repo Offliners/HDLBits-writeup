5cda09d401aa21cc6f0a7d1af5d0ac37294f557c
Offliner <42777325+Offliners@users.noreply.github.com>
2021-02-26 21:35:48 +0800

    Create 9.v

---
+++ Verilog Language/9/9.v
@@ -0,0 +1,15 @@
+`default_nettype none
+module top_module(
+    input a,
+    input b,
+    input c,
+    input d,
+    output out,
+    output out_n   ); 
+    wire w1, w2, w3;
+    assign w1 = a && b;
+    assign w2 = c && d;
+    assign w3 = w1 || w2;
+    assign {out, out_n} = {w3, ~w3};
+
+endmodule
