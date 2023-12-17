e2ebddcbd7190634f95daeab1dad3d971e96bc02
Offliners <eric21904@gmail.com>
2023-12-17 09:38:44 +0800

    update

--- Circuits/Sequential Logic/Latches and Flip-Flops/95/95.v
+++ Circuits/Sequential Logic/Latches and Flip-Flops/95/95.v
@@ -3,17 +3,11 @@ module top_module (
     input [7:0] in,
     output reg [7:0] pedge
 );
-    reg [7:0] in_temp;
+    reg [7:0] in_state;
     
     always @(posedge clk) begin
-        in_temp <= in;
-        
-        for(integer i = 0; i < 8; i = i + 1) begin
-            if((in_temp[i] == 0) && (in[i] == 1))
-                pedge[i] <= 1'b1;
-            else
-                pedge[i] <= 1'b0;
-        end
+        in_state <= in;
+        pedge <= in & ~in_state;
     end
 
 endmodule
